1d432aa8649bfe73059f2b9eb13164268e3ff4ab
Rowan <nope@email.com>
2026-09-14 22:36:41 -0700

    Seeded random generator; formatting & comments

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -34,6 +34,7 @@ bool promptForPlayAgain()
     std::cin >> response;
     if (response == 'y' || response == 'Y')
     {
+        std::cout << std::endl;
         return true;
     }
     else if (response == 'n' || response == 'N')
@@ -44,6 +45,7 @@ bool promptForPlayAgain()
     {
         std::cout << "invalid input - try again" << std::endl;
         promptForPlayAgain();
+        //get a warning that not all control paths return a value - but eventually will resolve to y/Y/n/N which will return a value
     }
 }
 
@@ -95,39 +97,9 @@ int main()
     //add space between test and game
     std::cout << std::endl;
 
-    //play initial round
+    //seed random number for game
+    srand( static_cast<unsigned int>(time(0)) );
+
+    //play game
     playOneRound();
 }
-
-// function guess number: 
-// initial condns
-// guess tracker = 0;
-// generate random number between 0 & 10 (inclusive)
-
-// c: '***Welcome to mystery number ***'
-// c: 'enter teh maximum range: '   ;    p: enter 10
-// c: ask user: guess the random number between 0 & 10 (inclusive) 
-// p: guess number 
-// c: if too high: guess lower
-// c: if too low: guess higher
-// c: add 1 to num guess tracker
-// c: if correct: 'correct - it tooy you ___ guesses'.
-//                  Do you wish to play again [y/n]: 
-// p: y or n
-// 
-
-
-//do { guessNumber() } while ( answer = y )
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
